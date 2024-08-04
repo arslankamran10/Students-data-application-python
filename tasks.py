7ea328cb40892e672996e0c5d77af241cca7f4e0
arslankamran10 <0arslm84@solent.ac.uk>
2024-08-04 16:44:39 +0100

    module b2
    b2 tasks almost completed

--- tasks.py
+++ tasks.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[5]:
+
+
+def welcome():
+    print("\n\n\t\t\t\t\t Welcome to US high school student records. \t\n")
+    print("\nChoose from the following options[1-5]:\n\n 1-Retrieve the sex, age, number of relative in a family, state, and race based on the ID.\n 2-Retrieve the sex, school support, access internet, attendance rate, and parental involvement associated with a specific race.\n 3-Retrieve the ID, free time, math score, reading score and writing score of students whose absences are less than 50 based on the parental involvement. \n 4-Retrieve traveltime,studytime and failure of students who got school support, associated with guardian \n 5-More \n")
+    return (" ")
+
 
 
 def a1(csv_reader,id):
@@ -21,7 +28,8 @@ def a2(csv_reader,race):
         #return result
         print(*result, sep = "\n")
         return("")
-    return("Race not found")
+    else:
+        return("Race not found")
 
 
 def a3(csv_reader,involvement):
@@ -45,6 +53,16 @@ def a4(csv_reader,guardian):
         print(*result3, sep = "\n")
         return("")
     return("Enter correct guardian")
+
+
+def b2(students_df, parental_involvement):
+    if parental_involvement=="high" or parental_involvement=="low" or parental_involvement=="medium":
+        parental_involvement_data = students_df[students_df['Parental_involvement'] == parental_involvement]
+        avg_absences = parental_involvement_data['Absences'].mean()
+        print(f"Average for {parental_involvement} parental involvement is:")
+        return avg_absences
+    else:
+        return("Enter correct involvement.")
 
 
 # In[ ]:
